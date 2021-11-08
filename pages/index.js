8c7a3e54c58df4f7956658287b6ec96642b98460
cdelacombaz <cdelacombaz@bluewin.ch>
2021-11-08 10:25:22 +0100

    Highest version working tested

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,3 @@
-const Home = () => (
-  <main>
-    <h1 className="title">
-      Welcome to <a href="https://nextjs.org">Next.js!!!</a>
-    </h1>
+const Home = () => <h1 className="title">Welcome to Next.js!!!</h1>
 
-    <p className="description">
-      Get started by editing <code>pages/index.js</code>
-    </p>
-  </main>
-);
-
-export default Home;
+export default Home
